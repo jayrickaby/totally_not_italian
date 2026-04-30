1e59280cab31ce557b32843628c1d9d809578d53
Jay <jayrickaby@pm.me>
2026-04-30 21:10:54 +0100

    feat: John's movement now uses a deltaTime clock so it is no longer framerate dependent

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,15 @@ int main() {
 
     window.setView(camera);
 
-    const float JOHN_SPEED {0.25};
+    const float JOHN_SPEED {50.f};
 
     sf::Texture johnTexture("assets/textures/john/sheet_john_main.png");
     sf::Sprite johnSprite{johnTexture};
 
     sf::IntRect johnIdleRect({0,0},{24,24});
     johnSprite.setTextureRect(johnIdleRect);
+
+    sf::Clock clock;
 
     while (window.isOpen()) {
         while (const std::optional event{window.pollEvent()})
@@ -24,11 +26,13 @@ int main() {
             }
         }
 
+        float deltaTime = clock.restart().asSeconds();
+
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
-            johnSprite.setPosition({johnSprite.getPosition().x - JOHN_SPEED, johnSprite.getPosition().y});
+            johnSprite.setPosition({johnSprite.getPosition().x - JOHN_SPEED * deltaTime, johnSprite.getPosition().y});
         }
         else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
-            johnSprite.setPosition({johnSprite.getPosition().x + JOHN_SPEED, johnSprite.getPosition().y});
+            johnSprite.setPosition({johnSprite.getPosition().x + JOHN_SPEED * deltaTime, johnSprite.getPosition().y});
         }
 
         window.clear();
