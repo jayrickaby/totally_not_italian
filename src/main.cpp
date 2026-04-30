ff810450cf7b2d98c8753fa368db250104a89560
Jay <jayrickaby@pm.me>
2026-04-30 16:11:37 +0100

    feat: Window now opens

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,22 @@
-int main() {
+#include <SFML/Window.hpp>
 
+#include "SFML/Graphics.hpp"
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode({1280, 720}), "Totally Not Italian");
+
+    while (window.isOpen()) {
+        while (const std::optional event{window.pollEvent()})
+        {
+            // Close window: exit
+            if (event->is<sf::Event::Closed>()) {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.display();
+    }
+
+    return 0;
 }
