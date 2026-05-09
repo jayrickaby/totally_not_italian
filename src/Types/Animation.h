fc80d58ff9afa7759814fa15075d05cbeb654b1f
Jay Rickaby <jayrickaby@pm.me>
2026-05-09 15:14:57 +0100

    fix: Properly placed Animation struct in header guard

--- src/Types/Animation.h
+++ src/Types/Animation.h
@@ -8,11 +8,12 @@
 
 #include "SFML/Graphics/Rect.hpp"
 
-#endif //TOTALLY_NOT_ITALIAN_ANIMATION_H
-
 struct Animation {
     std::vector<sf::IntRect> frames;
     float frameDuration = 1;
     float frameTimer = 0;
     signed int currentFrame = 0;
 };
+
+#endif //TOTALLY_NOT_ITALIAN_ANIMATION_H
+
